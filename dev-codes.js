cce81357edb6c77adee3f486e299dc076134a1c4
MoonLight <carloss.jubran@gmail.com>
2026-09-23 16:19:41 +0300

    Delete dev-codes.js

--- dev-codes.js
+++
@@ -1,3 +0,0 @@
-// Two-step code gate: BOTH codes are required, in order.
-export const DEV_CODE_1 = "TheBeastcLCIKERrawo123996767676234gA@Rjorjp";
-export const DEV_CODE_2 = "M0A-second-defense-code-9f3kD82xQ";
